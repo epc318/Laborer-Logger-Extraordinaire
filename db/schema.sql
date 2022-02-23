965a27618586ef7e30947ab80b51898ea7fa1ff8
Rick Johnson <carl4917@umn.edu>
2022-02-22 20:12:11 -0600

    editing schema.sql, creating routes folder a intial files

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,34 @@
+DROP DATABASE IF EXISTS laborer_logger;
 CREATE DATABASE IF NOT EXISTS laborer_logger;
 USE laborer_logger;
+
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    dept_id INT,
+    CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES dept(id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
